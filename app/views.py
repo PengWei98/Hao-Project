fead89fc9ec83e65642c583ff51cc326f2c17451
Peng Wei <3160104618@zju.edu.cn>
2019-09-16 00:20:23 +0800

    jquery

--- app/views.py
+++ app/views.py
@@ -3,12 +3,13 @@
 # @Author  : Wei Peng
 # @FileName: views.py
 
-from flask import request, render_template, flash
+from flask import request, render_template, flash, jsonify
 from werkzeug.utils import secure_filename
 import os
 from app import app
 import tableuni
 import uuid
+import json
 
 
 @app.route('/upload', methods=['POST'])
@@ -19,22 +20,33 @@ def upload_file():
     print(upload_path)
     upload_path = os.path.join(upload_path, secure_filename(f.filename))
     id = str(uuid.uuid1())
-    pdf_path = upload_path.split(".")[0] + "-" + id + ".pdf" # the unique name of the file
+    pdf_path = upload_path.split(".")[0] + "-" + id + ".pdf"  # the unique name of the file
     f.save(pdf_path)
     csv_path = upload_path.split(".")[0] + "-" + id + ".csv"
     # open(csv_path, "w")
     png_path = upload_path.split(".")[0] + "-" + id + ".png"
     # open(png_path, "w")
-    table = tableuni.with_table(pdf_path, "/Users/pengwei/PycharmProjects/Hao-Project/app/static/uploads/test.csv", "/Users/pengwei/PycharmProjects/Hao-Project/app/static/uploads/table.png")
+    table = tableuni.with_table(pdf_path, "/Users/pengwei/PycharmProjects/Hao-Project/app/static/uploads/test.csv",
+                                "/Users/pengwei/PycharmProjects/Hao-Project/app/static/uploads/table.png")
     # print(type(table))
     # print(table)
-    # print(table.index.values[0])
-    flash('Your file has been uploaded!', 'success')
-    return render_template('index.html', show=True, table=table)
+    print(table)
+    print(table.index.values[0])
+    table_json = {}
+    table_json["table"] = []
+    for row_id in table.index:
+        row = []
+        for item in table.loc[row_id].values:
+            row.append(item)
+
+        print(row)
+        table_json["table"].append(row)
+    # return render_template('index.html', show=True, table=table)
+    print(table_json)
+    x = jsonify(table_json)
+    return x
 
 
 @app.route('/', methods=['GET', "POST"])
 def index():
-    print("hello")
-    flash('Hello, test flash', 'success')
-    return render_template('index.html', show=False, table=None)
+    return render_template('index.html')
