cb8abb5a458792568bd6ea4987035b2b9a28da2e
Peng Wei <3160104618@zju.edu.cn>
2019-09-27 07:52:38 +0800

    loading:

--- app/models.py
+++ app/models.py
@@ -7,10 +7,10 @@ class Tableuni(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     dataset = db.Column(db.String(64))
-    method = db.Column(db.String(32))
-    metric = db.Column(db.String(32))
-    score = db.Column(db.String(32))
-    filename = db.Column(db.String(32))
+    method = db.Column(db.String(64))
+    metric = db.Column(db.String(64))
+    score = db.Column(db.String(64))
+    filename = db.Column(db.String(64))
 
     def __init__(self, dataset, method, metric, score, filename):
         self.dataset = dataset
