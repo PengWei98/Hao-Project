92218a8dfa9205b489535e2ed898a1c55f9a2677
Peng Wei <3160104618@zju.edu.cn>
2019-09-26 19:46:15 +0800

    db set

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'notre_dame'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@167.71.248.105:3306/tableuni'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = '/static/uploads'
