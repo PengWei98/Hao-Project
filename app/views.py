a7f0fcc3596e3738a94d74dc0fee5eb28b8ad314
Peng Wei <3160104618@zju.edu.cn>
2019-09-29 20:09:51 +0800

    search

--- app/views.py
+++ app/views.py
@@ -107,6 +107,90 @@ def get_db_table():
     return render_template('index.html', state=2, db_table=db_table, remarkform=remarkform, id=id, fileform=fileform)
 
 
+@app.route('/search1', methods=['GET'])
+def search1():
+    text = request.args.get("text")
+    print("text:")
+    print(text)
+    methods = set()
+    results = Tableuni.query.filter_by(dataset=text).all()
+    for result in results:
+        if result.method.replace(" ", "") != "":
+            methods.add(result.method)
+    for method in methods:
+        print(method)
+    json = {}
+    json["methods"] = list(methods)
+    print(json)
+    return jsonify(json)
+
+
+@app.route('/search2', methods=['GET'])
+def search2():
+    text = request.args.get("text")
+    print("text:")
+    print(text)
+    metrics = set()
+    results = Tableuni.query.filter_by(dataset=text).all()
+    for result in results:
+        if result.metric.replace(" ", "") != "":
+            metrics.add(result.metric)
+    print(metrics)
+    json = {}
+    json["metrics"] = list(metrics)
+    print(json)
+    return jsonify(json)
+
+
+@app.route('/search3', methods=['GET'])
+def search3():
+    text = request.args.get("text")
+    print("text:")
+    print(text)
+    sources = set()
+    results = Tableuni.query.filter_by(dataset=text).all()
+    print("len:" + str(len(results)))
+    for result in results:
+        if result.filename.replace(" ", "") != "":
+            # print("replace" + result.filename.replace(" ", ""))
+            print("filename:")
+            print(result.filename)
+            sources.add(result.filename)
+    print(sources)
+    datasets = set()
+    for filename in sources:
+        results = Tableuni.query.filter_by(filename=filename)
+        for result in results:
+            if result.dataset.replace(" ", "") != "":
+                datasets.add(result.dataset)
+    json = {}
+    json["datasets"] = list(datasets)
+    print(json)
+    return jsonify(json)
+
+
+@app.route('/search4', methods=['GET'])
+def search4():
+    text1 = request.args.get("text1")
+    text2 = request.args.get("text2")
+    text3 = request.args.get("text3")
+    print("text:")
+    print(text1)
+    print(text2)
+    method_score = []
+    results = Tableuni.query.filter_by(dataset=text2).filter_by(metric=text3).order_by(
+        db.desc(Tableuni.score)).all()
+    for result in results:
+        if result.method.replace(" ", "") != "":
+            method_score.append((result.method, result.score))
+            if len(method_score) == int(text1):
+                break
+    json = {}
+    json["method_score"] = method_score
+    print(json)
+    return jsonify(json)
+
+
 @app.route('/', methods=['GET', "POST"])
 @app.route('/index', methods=['GET', "POST"])
 def index():
